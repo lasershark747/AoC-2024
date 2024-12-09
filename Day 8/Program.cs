0f1c41b190376ceda33784b5af670b5ef76a5ae9
Lasershark747 <mjstalley2209@gmail.com>
2024-12-09 18:29:01 +0000

    Day 9

---
+++ Day 8/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            long count = 0;
+            Dictionary<char, List<(int, int)>> antenna = new Dictionary<char, List<(int, int)>>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                for(int j =0;  j < input[i].Length; j++)
+                {
+                    if (input[i][j] != '.')
+                    {
+                        if (antenna.Keys.Contains(input[i][j]))
+                        {
+                            antenna[input[i][j]].Add((i, j));
+                        }
+                        else
+                        {
+                            List<(int,int)> temp = new List<(int, int)>();
+                            temp.Add((i,j));
+                            antenna.Add(input[i][j], temp);
+                        }
+                    }
+                }
+            }
+
+
+
+            //Part 1
+            List<(int,int)> intersections = new List<(int,int)> ();
+            foreach(var v in antenna.Values)
+            {
+                for(int i = 0;i< v.Count;i++)
+                {
+                    for(int j = i+1; j < v.Count; j++)
+                    {
+                        (int, int) iTOj = (v[i].Item1 - v[j].Item1, v[i].Item2 - v[j].Item2);
+                        (int, int) p1 = (v[j].Item1 - iTOj.Item1, v[j].Item2 - iTOj.Item2);
+                        (int, int) p2 = (v[i].Item1 + iTOj.Item1, v[i].Item2 + iTOj.Item2);
+
+                        if ((!intersections.Contains(p1)) && inside(p1, input.Length, input[0].Length))
+                        {
+                            intersections.Add(p1);
+                        }
+                        if (!(intersections.Contains(p2)) && inside(p2, input.Length, input[0].Length))
+                        {
+                            intersections.Add(p2);
+                        }
+                    }
+                }
+            }
+
+
+
+
+            Console.WriteLine("Part 1: " + intersections.Count);
+
+
+            //Part 2
+            count = 0;
+            intersections = new List<(int, int)>();
+            foreach (var v in antenna.Values)
+            {
+                for (int i = 0; i < v.Count; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        (int, int) iTOj = (v[i].Item1 - v[j].Item1, v[i].Item2 - v[j].Item2);
+                        (int, int) p1 = (v[j].Item1, v[j].Item2 );
+                        (int, int) p2 = (v[i].Item1, v[i].Item2);
+                        
+                        while (true) 
+                        {
+                            if (!inside(p1, input.Length, input[0].Length))
+                            {
+                                break;
+                            }
+                            if (!intersections.Contains(p1))
+                            {
+                                intersections.Add(p1);
+                            } 
+                            p1 = (p1.Item1 - iTOj.Item1, p1.Item2 - iTOj.Item2);
+                        }
+
+                        while (true)
+                        {
+                            if (!inside(p2, input.Length, input[0].Length))
+                            {
+                                break;
+                            }
+                            if (!intersections.Contains(p2))
+                            {
+                                intersections.Add(p2);
+                            }
+                            p2 = (p2.Item1 + iTOj.Item1, p2.Item2 + iTOj.Item2);
+                        }
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Part 2: " + intersections.Count);
+            Console.ReadKey();
+        }
+
+        public static bool inside((int,int) p, int i, int j)
+        {
+            if(p.Item1 < 0 || p.Item2 < 0)
+                return false;
+            if(p.Item1>=i || p.Item2 >= j)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Day 9/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            int count = 0;
+            int c = 0;
+
+
+            //Part 1
+            List<int> value = new List<int>();
+            List<int> stack = new List<int>();
+            bool full = true;
+            for (int i = 0; i < input[0].Length; i++)
+            {
+                for (int j = 0; j < int.Parse(input[0][i].ToString()); j++)
+                {
+                    if (full)
+                    {
+                        value.Add(1);
+                        stack.Add(count);
+                        c++;
+                    }
+                    else
+                    {
+                        value.Add(-1);
+                    }
+                }
+                if (full)
+                {
+                    count++;
+                }
+                full = !full;
+            }
+            count = 0;
+
+
+            List<int> values = new List<int>();
+            while(stack.Count > 0) 
+            {
+                if (value[count] == 1)
+                {
+                    values.Add(stack[0]);
+                    stack.RemoveAt(0);
+                }
+                else if(value[count] == -1)
+                {
+                    values.Add(stack[stack.Count-1]);
+                    stack.RemoveAt(stack.Count-1);
+                }
+                count++;
+            }
+
+            count = 0;
+            long t = 0;
+            for (int i = 0; i < values.Count; i++)
+            {
+
+                t += i * values[i];
+
+            }
+
+
+            Console.WriteLine("Part 1: " + t);
+
+            //Part 2
+            count = 0;
+            List<(int,int)> disk = new List<(int,int)>();
+            List<int> lens = new List<int>();
+            for (int i = 0; i < input[0].Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    disk.Add((count, int.Parse(input[0][i].ToString())));
+                    count++;
+                }
+                else
+                {
+                    disk.Add((-1, int.Parse(input[0][i].ToString())));
+                }
+            }
+
+            for (int i = disk.Count - 1; i >= 0; i--) 
+            {
+                if (disk[i].Item1 != -1)
+                {
+                    var v = disk[i];
+                    for (int j = 0; j < i; j++)
+                    {
+                        var v2 = disk[j];
+                        if (v2.Item1 == -1)
+                        {
+                            if (v2.Item2 == v.Item2)
+                            {
+                                disk[j] = v;
+                                disk[i] = v2;
+                                break;
+                            }
+                            else if (v2.Item2 > v.Item2)
+                            {
+                                int dif = v2.Item2 - v.Item2;
+                                disk[j] = v;
+                                disk.Insert(j + 1, (-1, dif));
+                                disk[i+1] = (-1, v.Item2);
+                                i++;
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            t = 0;
+            c = 0;
+
+            for(int i = 0; i < disk.Count; i++)
+            {
+                if (disk[i].Item1 != -1)
+                {
+                    for (int j = 0; j < disk[i].Item2; j++)
+                    {
+                        t += disk[i].Item1 * c;
+                        c++;
+                    }
+                }
+                else
+                {
+                    c += disk[i].Item2;
+                }
+            }
+
+            Console.WriteLine("Part 2: " + t);
+            Console.ReadKey();
+        }
+    }
+}
